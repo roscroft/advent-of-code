94a0b32b8f8d01d1b20387440d060362ba2e85b5
Austin <adherrling@gmail.com>
2018-12-03 23:03:12 -0500

    Finished part 2.

--- day3/soln.py
+++ day3/soln.py
@@ -4,34 +4,44 @@ def main():
     with open('input.txt', 'r+') as datafile:
         data = datafile.readlines()
 
-    def part1():
-        num_regex = re.compile(r'#\d+ @ (\d+),(\d+): (\d+)x(\d+)')
-        data_tups = []
-        for item in data:
-            num_match = num_regex.match(item)
-            nums = num_match.groups()
-            data_tups.append(nums)
+    num_regex = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+    data_tups = []
+    for item in data:
+        num_match = num_regex.match(item)
+        nums = num_match.groups()
+        data_tups.append(nums)
 
+    def gen_coordinates(left, top, wide, tall):
+        coords = set()
+        for i in range(left+1, left+wide+1):
+            for j in range(top+1, top+tall+1):
+                coords.add((i,j))
+        return coords
+
+    def part1and2():
         seen_set = set()
         collided_set = set()
 
-        def gen_coordinates(left, top, wide, tall):
-            coords = set()
-            for i in range(left+1, left+wide+1):
-                for j in range(top+1, top+tall+1):
-                    coords.add((i,j))
-            return coords
-
-        for left, top, wide, tall in data_tups:
+        for idx, left, top, wide, tall in data_tups:
             coords = gen_coordinates(int(left), int(top), int(wide), int(tall))
             for coord in coords:
                 if coord in seen_set:
                     collided_set.add(coord)
                 else:
                     seen_set.add(coord)
-        
-        return len(collided_set)
 
-    print(part1())
+        print(len(collided_set))
+
+        for idx, left, top, wide, tall in data_tups:
+            coords = gen_coordinates(int(left), int(top), int(wide), int(tall))
+            collided = False
+            for coord in coords:
+                if coord in collided_set:
+                    collided = True
+                    continue
+            if collided == False:
+                return idx
+
+    print(part1and2())
 if __name__ == "__main__":
     main()
