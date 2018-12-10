7fe2f48dfd12eef381065ef88604463f2634baf1
Austin <adherrling@gmail.com>
2018-12-09 21:00:15 -0500

    Day 6 not working yet.

---
+++ day6/soln.py
@@ -0,0 +1,81 @@
+def main():
+    with open('input.txt', 'r+') as datafile:
+        data = datafile.readlines()
+
+    def manhattan(coord_1, coord_2):
+        return abs(int(coord_1[0]) - int(coord_2[0])) + abs(int(coord_1[1]) - int(coord_2[1]))
+
+    def argmin(distances):
+        min_idx = 0
+        min_dist = 100000000
+        for idx, dist in distances:
+            if dist < min_dist:
+                min_dist = dist
+                min_idx = idx
+        return min_idx
+
+    def get_distances(coord, area_dict):
+        distances = []
+        for key, value in area_dict.items():
+            # if not value["inf"]:
+            ref_point = [value["x"], value["y"]]
+            distances.append((key, manhattan(coord, ref_point)))
+        return distances
+
+    def elementwise_more(list_1, list_2):
+        for i in range(len(list_1)):
+            if list_1[i] < list_2[i]:
+                return False
+        return True
+
+    def part1(data):
+        area_dict = {}
+        idx = 0
+        for item in data:
+            coords = item.strip().split(", ")
+            area_dict[idx] = {"x": coords[0], "y": coords[1], "area": 0, "inf": False}
+            idx += 1
+
+        start = (0, 0)
+        coord_dict = {}
+        to_explore = set()
+        explored = set()
+        to_explore.add(start)
+        last_distances = get_distances(start, area_dict)
+        coord_dict[start] = last_distances
+        closest_point = argmin(last_distances)
+        area_dict[closest_point]["area"] += 1
+
+        while to_explore:
+            cur = to_explore.pop()
+            last_distances = coord_dict[cur]
+            coord_dict.pop(cur)
+
+            up = (cur[0], cur[1]+1)
+            down = (cur[0], cur[1]-1)
+            left = (cur[0]-1, cur[1])
+            right = (cur[0]+1, cur[1])
+            for direction in [up, down, left, right]:
+                if direction not in explored:
+                    new_distances = get_distances(direction, area_dict)
+                    closest_point = argmin(new_distances)
+                    if elementwise_more(new_distances, last_distances):
+                        area_dict[closest_point]["inf"] = True
+                    else:
+                        area_dict[closest_point]["area"] += 1
+                        to_explore.add(direction)
+                        coord_dict[direction] = new_distances
+                    explored.add(direction)
+
+        max_area = 0
+        for key, value in area_dict.items():
+            area = value["area"]
+            is_inf = value["inf"]
+            if area > max_area and not is_inf:
+                max_area = area
+
+        print(max_area)
+
+    part1(data)
+if __name__ == "__main__":
+    main()
