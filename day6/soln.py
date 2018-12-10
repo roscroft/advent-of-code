8a45a389013de6dba72fc7dacabab3d79023eef7
Austin <adherrling@gmail.com>
2018-12-10 07:24:05 -0500

    Both parts work but I don't feel good about them.

--- day6/soln.py
+++ day6/soln.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def main():
     with open('input.txt', 'r+') as datafile:
         data = datafile.readlines()
@@ -8,16 +10,21 @@ def main():
     def argmin(distances):
         min_idx = 0
         min_dist = 100000000000
+        seen = False
         for idx, dist in distances:
+            if dist == min_dist:
+                seen = True
             if dist < min_dist:
                 min_dist = dist
                 min_idx = idx
+                seen = False
+        if seen:
+            return None
         return min_idx
 
     def get_distances(coord, area_dict):
         distances = []
         for key, value in area_dict.items():
-            # if not value["inf"]:
             ref_point = [value["x"], value["y"]]
             distances.append((key, manhattan(coord, ref_point)))
         return distances
@@ -27,6 +34,9 @@ def main():
             if list_1[i] < list_2[i]:
                 return False
         return True
+
+    def tuplesum(tup_lst):
+        return sum(n for _, n in tup_lst)
 
     def part1(data):
         area_dict = {}
@@ -41,6 +51,7 @@ def main():
         to_explore = set()
         explored = set()
         to_explore.add(start)
+        explored.add(start)
         last_distances = get_distances(start, area_dict)
         coord_dict[start] = last_distances
         closest_point = argmin(last_distances)
@@ -60,9 +71,11 @@ def main():
                     new_distances = get_distances(direction, area_dict)
                     closest_point = argmin(new_distances)
                     if elementwise_more(new_distances, last_distances):
-                        area_dict[closest_point]["inf"] = True
+                        if closest_point is not None:
+                            area_dict[closest_point]["inf"] = True
                     else:
-                        area_dict[closest_point]["area"] += 1
+                        if closest_point is not None:
+                            area_dict[closest_point]["area"] += 1
                         to_explore.add(direction)
                         coord_dict[direction] = new_distances
                     explored.add(direction)
@@ -74,9 +87,61 @@ def main():
             if area > max_area and not is_inf:
                 max_area = area
 
-        print(len(explored))
-        print(max_area)
+        return max_area
 
-    part1(data)
+    def part2(data):
+        area_dict = {}
+        idx = 0
+        for item in data:
+            coords = item.strip().split(", ")
+            area_dict[idx] = {"x": coords[0], "y": coords[1], "area": 0}
+            idx += 1
+
+        part2_size = 0
+        start = (0, 0)
+        coord_dict = {}
+        to_explore = set()
+        explored = set()
+        to_explore.add(start)
+        explored.add(start)
+        last_distances = get_distances(start, area_dict)
+        print(last_distances)
+        print(tuplesum(last_distances))
+        if tuplesum(last_distances) < 10000:
+            part2_size += 1
+        coord_dict[start] = last_distances
+        closest_point = argmin(last_distances)
+        area_dict[closest_point]["area"] += 1
+
+        while to_explore:
+            cur = to_explore.pop()
+            last_distances = coord_dict[cur]
+            coord_dict.pop(cur)
+
+            up = (cur[0], cur[1]+1)
+            down = (cur[0], cur[1]-1)
+            left = (cur[0]-1, cur[1])
+            right = (cur[0]+1, cur[1])
+            for direction in [up, down, left, right]:
+                if direction not in explored:
+                    new_distances = get_distances(direction, area_dict)
+                    closest_point = argmin(new_distances)
+                    if elementwise_more(new_distances, last_distances):
+                        if closest_point is not None:
+                            area_dict[closest_point]["inf"] = True
+                        if tuplesum(new_distances) < 10000:
+                            part2_size += 1
+                    else:
+                        if closest_point is not None:
+                            area_dict[closest_point]["area"] += 1
+                        if tuplesum(new_distances) < 10000:
+                            part2_size += 1
+                        to_explore.add(direction)
+                        coord_dict[direction] = new_distances
+                    explored.add(direction)
+
+        return part2_size
+
+    print(part2(data))
 if __name__ == "__main__":
     main()
